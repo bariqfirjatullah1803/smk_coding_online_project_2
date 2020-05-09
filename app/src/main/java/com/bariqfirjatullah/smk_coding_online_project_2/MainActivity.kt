f595fe919b2a9bc6a13be5449079731cd3e047e4
bariqfirjatullah1803 <bariqfirjatullah1803@gmail.com>
2020-05-09 17:07:30 +0700

    Project Created

---
+++ app/src/main/java/com/bariqfirjatullah/smk_coding_online_project_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bariqfirjatullah.smk_coding_online_project_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
